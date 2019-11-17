fb53c9281bee3a83d573796a2e90382dc963ed90
Chris <christopher0chung@gmail.com>
2019-11-17 16:18:16 -0500

    Reoptimized for laptop (2k E8th), and scene configurator.

---
+++ LookingGalssCloudsV2/Crewcible/Assets/Test_SceneConfigurator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_SceneConfigurator : MonoBehaviour
+{
+    public bool isNormal;
+    public Color normal;
+    public Color happy;
+    public LookingGlass.Holoplay hP;
+
+    public GameObject normalClouds;
+    public GameObject happyClouds;
+
+    public Camera[] miniCams;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isNormal)
+        {
+            hP.background = normal;
+            RenderSettings.fogColor = normal;
+            Instantiate(normalClouds, Vector3.zero, Quaternion.identity, ServiceLocator.instance.View);
+            foreach (Camera c in miniCams)
+                c.backgroundColor = normal;
+        }
+        else
+        {
+            hP.background = happy;
+            RenderSettings.fogColor = happy;
+            Instantiate(happyClouds, Vector3.zero, Quaternion.identity, ServiceLocator.instance.View);
+            foreach (Camera c in miniCams)
+                c.backgroundColor = happy;
+        }
+    }
+}
